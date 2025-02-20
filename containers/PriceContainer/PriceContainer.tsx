607ba38c315c336178fa965dd5663e8b2d581c23
Adam Griffin <ag@agriffin.me>
2025-02-19 17:34:28 -0800

    Got price range working

--- containers/PriceContainer/PriceContainer.tsx
+++ containers/PriceContainer/PriceContainer.tsx
@@ -1,35 +1,43 @@
-import { useAppDispatch, useAppSelector } from "../../hooks/app_hooks";
-import { useRunCallbackIfChanged } from "../../hooks/run_callback_if_changed_hook";
-import { selectPriceRange } from "../../store/filters/filtersSelectors";
-import { setFilters } from "../../store/filters/filtersSlice";
-import { searchWithUpdatedFilters } from "../../store/listingSearch/listingSearchCommon";
-import PriceRange from "../../components/form/PriceRange/PriceRange";
+import { useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import { useUpdateSearchParams } from '~/hooks/useUpdateSearchParams'
+import type { PriceRangeFilters } from '~/store/filters/filtersTypes'
+import PriceRange from '../../components/form/PriceRange/PriceRange'
 
-/**
- * A Container component to connect the <PriceRange> component to the store.
- */
 const PriceContainer: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const priceRange = useAppSelector(selectPriceRange);
-  const [setPreviousPriceRange, runSearchIfPriceRangeChanged] =
-    useRunCallbackIfChanged(priceRange, () =>
-      dispatch(searchWithUpdatedFilters())
-    );
+  const searchParams = useSearchParams()
+  const updateSearchParams = useUpdateSearchParams()
+  const [priceRange, setPriceRange] = useState<PriceRangeFilters>({
+    priceMin: Number(searchParams.get('price_min')) || null,
+    priceMax: Number(searchParams.get('price_max')) || null
+  })
 
+  // The onChange event keeps track of the current state of each price field, so
+  // we're tracking that locally with useState, but we only want to update the
+  // url and trigger a new fetch request with updateSearchParams when the onBlur
+  // or onMenuItemSelected events occur.
   return (
     <PriceRange
       priceRange={priceRange}
-      onChange={(priceRange) => {
-        dispatch(setFilters(priceRange));
+      onChange={(newPriceRange) => {
+        setPriceRange({ ...priceRange, ...newPriceRange })
       }}
-      onFocus={setPreviousPriceRange}
-      onBlur={runSearchIfPriceRangeChanged}
-      onMenuItemSelected={(priceRange) => {
-        dispatch(setFilters(priceRange));
-        dispatch(searchWithUpdatedFilters());
+      onBlur={() =>
+        updateSearchParams({
+          price_min: priceRange.priceMin,
+          price_max: priceRange.priceMax
+        })
+      }
+      onMenuItemSelected={(selectedPriceRange) => {
+        const update = { ...priceRange, ...selectedPriceRange }
+        setPriceRange(update)
+        updateSearchParams({
+          price_min: update.priceMin || null,
+          price_max: update.priceMax || null
+        })
       }}
     />
-  );
-};
+  )
+}
 
-export default PriceContainer;
+export default PriceContainer
