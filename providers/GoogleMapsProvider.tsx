b1f0bf19248050629c1b13641942946827a0aad7
Adam Griffin <ag@agriffin.me>
2025-02-21 15:12:19 -0800

    Mark ReactNode as a type

--- providers/GoogleMapsProvider.tsx
+++ providers/GoogleMapsProvider.tsx
@@ -1,50 +1,50 @@
-"use client";
+'use client'
 
 import {
   createContext,
   useContext,
   useState,
   useEffect,
-  ReactNode
-} from "react";
-import type { Dispatch } from "react";
-import type { LoaderOptions, Libraries } from "@googlemaps/js-api-loader";
-import { Loader } from "@googlemaps/js-api-loader";
+  type ReactNode
+} from 'react'
+import type { Dispatch } from 'react'
+import type { LoaderOptions, Libraries } from '@googlemaps/js-api-loader'
+import { Loader } from '@googlemaps/js-api-loader'
 import {
   DefaultGoogleMapsLoaderOptions,
   DefaultGoogleMapsLibraries
-} from "../config/googleMapsOptions";
+} from '../config/googleMapsOptions'
 
 export type GoogleMapsContextInterface = {
-  googleLoaded: boolean;
-  googleMap: google.maps.Map | null;
-  setGoogleMap: Dispatch<google.maps.Map>;
-};
+  googleLoaded: boolean
+  googleMap: google.maps.Map | null
+  setGoogleMap: Dispatch<google.maps.Map>
+}
 
 export type GoogleMapsProviderProps = {
-  children: ReactNode;
-  loaderOptions?: LoaderOptions;
-  libraries?: Libraries;
-};
+  children: ReactNode
+  loaderOptions?: LoaderOptions
+  libraries?: Libraries
+}
 
 const GoogleMapsContext = createContext<GoogleMapsContextInterface>({
   googleLoaded: false,
   googleMap: null,
   setGoogleMap: () => {}
-});
+})
 
 // Custom hook to use with GoogleMapsProvider
-export const useGoogleMaps = () => useContext(GoogleMapsContext);
+export const useGoogleMaps = () => useContext(GoogleMapsContext)
 
 const GoogleMapsProvider: React.FC<GoogleMapsProviderProps> = ({
   loaderOptions = DefaultGoogleMapsLoaderOptions,
   libraries = DefaultGoogleMapsLibraries,
   children
 }) => {
-  const [googleLoaded, setGoogleLoaded] = useState(false);
+  const [googleLoaded, setGoogleLoaded] = useState(false)
   // This will be set later by the <GoogleMap> component since it generates the
   // div we need to create the map instance
-  const [googleMap, setGoogleMap] = useState<google.maps.Map | null>(null);
+  const [googleMap, setGoogleMap] = useState<google.maps.Map | null>(null)
 
   // There is no npm module for the Google Maps API. You have to load it via a
   // script tag. The Loader from @googlemaps/js-api-loader just creates a nice
@@ -53,24 +53,24 @@ const GoogleMapsProvider: React.FC<GoogleMapsProviderProps> = ({
   // execute whatever code depends on the API after the promise resolves.
   useEffect(() => {
     const initializeGoogleMaps = async () => {
-      const loader = new Loader(loaderOptions);
-      await Promise.all(libraries.map((l) => loader.importLibrary(l)));
-      setGoogleLoaded(true);
-    };
+      const loader = new Loader(loaderOptions)
+      await Promise.all(libraries.map((l) => loader.importLibrary(l)))
+      setGoogleLoaded(true)
+    }
     initializeGoogleMaps().catch((error) =>
-      console.error("Unable to load Google Maps API", error)
-    );
-  }, [libraries, loaderOptions]);
+      console.error('Unable to load Google Maps API', error)
+    )
+  }, [libraries, loaderOptions])
 
   // Passing the specific data we want as an object to the value prop in the
   // Provider component is what makes it available to child components that use
   // the context
-  const value = { googleLoaded, googleMap, setGoogleMap };
+  const value = { googleLoaded, googleMap, setGoogleMap }
   return (
     <GoogleMapsContext.Provider value={value}>
       {children}
     </GoogleMapsContext.Provider>
-  );
-};
+  )
+}
 
-export default GoogleMapsProvider;
+export default GoogleMapsProvider
