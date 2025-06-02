f23e9a2099a14ba96d619d0ef53b6d83f70c54bb
Adam Griffin <ag@agriffin.me>
2025-06-02 13:22:14 -0700

    Fixed PriceRange test

--- components/form/PriceRange/PriceRange.test.tsx
+++ components/form/PriceRange/PriceRange.test.tsx
@@ -2,15 +2,13 @@ import { render, screen } from '@testing-library/react'
 import PriceRange from './PriceRange'
 
 describe('PriceRange', () => {
-  
   it('Includes a Min Price field', () => {
     render(<PriceRange priceRange={{ priceMin: null, priceMax: null }} />)
-    expect(screen.getByRole('textbox', { name: 'Min Price' })).toBeInTheDocument()
+    expect(screen.getByLabelText('Min Price')).toBeInTheDocument()
   })
 
   it('Includes a Max Price field', () => {
-    render(<PriceRange priceRange={{ priceMin: null, priceMax: null }}  />)
-    expect(screen.getByRole('textbox', { name: 'Max Price' })).toBeInTheDocument()
+    render(<PriceRange priceRange={{ priceMin: null, priceMax: null }} />)
+    expect(screen.getByLabelText('Max Price')).toBeInTheDocument()
   })
-
 })
