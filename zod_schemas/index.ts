d9e5ad28267ab5535d680c4a539e3bcabb9cd294
Adam Griffin <ag@agriffin.me>
2025-08-12 17:39:17 -0700

    Added parseAndStripInvalidProperties
    Used to remove invalid properties from the url

--- zod_schemas/index.ts
+++ zod_schemas/index.ts
@@ -1,4 +1,18 @@
 import { z } from "zod";
+import { type ZodObject, type ZodRawShape } from "zod";
+
+export function parseAndStripInvalidProperties<
+  T extends ZodObject<ZodRawShape>
+>(schema: T, obj: object): Partial<z.infer<T>> {
+  const result = schema.safeParse(obj);
+
+  if (result.success) return result.data;
+
+  const invalidKeys = new Set(result.error.issues.map((i) => i.path[0]));
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) => !invalidKeys.has(key))
+  ) as Partial<z.infer<T>>;
+}
 
 export const booleanEnum = z
   .enum(["true", "false"])
