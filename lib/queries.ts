889dca1829966f1b743eebfb8e19edcae18661a7
Adam Griffin <ag@agriffin.me>
2025-08-13 14:31:21 -0700

    Fixed bug with searchQueryOptions
    We were previously using toString() with a ReadonlyURLSearchParams, which worked fine, but now that we have switched to an object this no longer serializes it correctly. It seems that we don't really need to serialize the object. Its properties won't be changed because it has been frozen with Object.freeze() and the only time its reference will change is if the underlying searchParams changes. The docs also mention that the order of keys doesn't matter.

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,12 +1,15 @@
 import { queryOptions } from "@tanstack/react-query";
-import { SearchState } from "~/zod_schemas/searchStateSchema";
 import { fetchListings } from "~/lib/fetchListings";
+import { type SearchState } from "~/zod_schemas/searchStateSchema";
 
-export function searchQueryOptions(searchParams: SearchState) {
+export function searchQueryOptions(searchState: SearchState) {
   return queryOptions({
-    // Run fetchListings() every time search params change
-    queryKey: ["search", searchParams.toString()],
-    queryFn: () => fetchListings(searchParams),
+    // Run fetchListings() every time searchState changes. We're tracking based
+    // on the object reference change because the object is read-only and should
+    // only be able to change when the underlying searchParams url object it
+    // depends on changes.
+    queryKey: ["search", searchState],
+    queryFn: () => fetchListings(searchState),
     staleTime: 1000 * 60
   });
 }
