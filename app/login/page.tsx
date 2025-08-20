c6ace47bfa7329cdea874e1308e414cfc1ce47a3
Adam Griffin <ag@agriffin.me>
2025-08-19 18:47:38 -0700

    Removed empty fallback function
    Looks to be pointless

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,10 +2,6 @@ import { Suspense } from "react";
 import LoginOrRegisterForm from "../../components/form/LoginOrRegisterForm/LoginOrRegisterForm";
 import styles from "./login.module.css";
 
-function LoginOrRegisterFormFallback() {
-  return <></>;
-}
-
 const Login: React.FC = () => {
   return (
     <div className={styles.page}>
@@ -14,7 +10,7 @@ const Login: React.FC = () => {
           <h1 className={styles.heading}>Login or Sign Up</h1>
         </header>
         <div className={styles.formContainer}>
-          <Suspense fallback={<LoginOrRegisterFormFallback />}>
+          <Suspense>
             <LoginOrRegisterForm useCallbackUrl />
           </Suspense>
         </div>
