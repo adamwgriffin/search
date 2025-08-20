a0e7d0ee78e54c6e22aec0a759744f58fc31b57f
Adam Griffin <ag@agriffin.me>
2025-08-19 18:45:34 -0700

    Formatting

--- app/api/current_user/route.ts
+++ app/api/current_user/route.ts
@@ -10,8 +10,9 @@ export type UserQueryResult = Pick<
   "id" | "name" | "email" | "image" | "image" | "favoriteIds"
 > | null;
 
-// There's currently a bug in the type definitions for GET that causes a Typescript error when we build the project.
-// Explicitly typing the return value for the function allows us to work around this.
+// There's currently a bug in the type definitions for GET that causes a
+// Typescript error when we build the project. Explicitly typing the return
+// value for the function allows us to work around this.
 export async function GET(): Promise<NextResponse<UserQueryResult>> {
   const session = await getServerSession(authOptions);
   if (!session?.user) {
