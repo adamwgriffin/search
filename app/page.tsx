74ab36aa6017c7563f825d23237e31b738e57b2a
Adam Griffin <ag@agriffin.me>
2026-08-16 16:10:39 -0700

    Workaround for Suspense boundary error
    Merging the latest from the master branch introduced an error with the newer version of Next.js that was included. Next will now throw a build error if you use the useSearchParams hook without a Suspense boundary around it. We wrapped the entire / route in <Suspense> as a workaround, which is not ideal since the entire main route will be dynamic and therefore can't be prerendered.

--- app/page.tsx
+++ app/page.tsx
@@ -11,25 +11,30 @@ import styles from "./page.module.css";
 import ReactQueryClientProvider from "@/providers/ReactQueryClientProvider";
 import { Suspense } from "react";
 
+// TODO: We're using the <Suspense> below as a temporary workaround for the fact that
+// SearchStateProvider uses the useSearchParams hook, which will opt the entire
+// page into client-side rendering. A better solution would be to isolate any
+// code that requires useSearchParams and move it into the child client
+// components that need it, but there is so much code involed that doing this
+// would take a major refactor. See
+// https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
 const SearchPage: NextPage = () => {
   return (
     <GoogleMapsProvider>
-      <SearchStateProvider>
-        <ReactQueryClientProvider>
-          <div className={styles.search}>
-            <SearchHeader />
-            <div className={styles.results}>
-              <Suspense>
+      <ReactQueryClientProvider>
+        <Suspense>
+          <SearchStateProvider>
+            <div className={styles.search}>
+              <SearchHeader />
+              <div className={styles.results}>
                 <SearchResults />
-              </Suspense>
-              <Suspense>
                 <ListingMap />
-              </Suspense>
+              </div>
+              <SearchModals />
             </div>
-            <SearchModals />
-          </div>
-        </ReactQueryClientProvider>
-      </SearchStateProvider>
+          </SearchStateProvider>
+        </Suspense>
+      </ReactQueryClientProvider>
     </GoogleMapsProvider>
   );
 };
